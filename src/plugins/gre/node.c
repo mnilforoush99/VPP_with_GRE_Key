fd9c894fa99e987f8ba354cdae510e2ce3d099b8
Masih Nilforoush <m.nilforoush@x-ion.de>
2025-02-17 17:24:34 +0100

    addig debug prints in node.c

--- src/plugins/gre/node.c
+++ src/plugins/gre/node.c
@@ -103,6 +103,8 @@ gre_tunnel_get (const gre_main_t *gm, vlib_node_runtime_t *node,
   const uword *p;
   p = is_ipv6 ? hash_get_mem (gm->tunnel_by_key6, &key->gtk_v6) :
 		      hash_get_mem (gm->tunnel_by_key4, &key->gtk_v4);
+  // debug
+  clib_warning("Tunnel lookup result: %d", p ? 1 : 0);
   if (PREDICT_FALSE (!p))
     {
       *next = GRE_INPUT_NEXT_DROP;
@@ -404,6 +406,13 @@ gre_input (vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame,
                 vnet_buffer(b[0])->ip.fib_index, type[0],
                 TUNNEL_MODE_P2P, 0, gre_key, &key[0].gtk_v4);
 	  matched[0] = gre_match_key4 (&cached_key.gtk_v4, &key[0].gtk_v4);
+    //Debug
+    clib_warning("GRE tunnel lookup - src: %U dst: %U fib: %d type: %d key: %d",
+      format_ip4_address, &ip4[0]->src_address,
+      format_ip4_address, &ip4[0]->dst_address,
+      vnet_buffer(b[0])->ip.fib_index,
+      type[0],
+      gre_key);
 	}
 
       tun_sw_if_index[0] = cached_tun_sw_if_index;
